59f91e02764bbcf1671a9596054dc986eb8080b2
Daniel Maximov <87207207+Danielmaximov123@users.noreply.github.com>
2022-04-01 14:22:31 +0300

    Delete src directory

--- src/post.js
+++
@@ -1,9 +0,0 @@
-const PostComp = (props) => {
-
-    return <div className="postItem">
-    <span className="post">Title : {props.posts.title}</span> <br/>
-    <span className="post">Body : {props.posts.body}</span>
-  </div>;
-}
-
-export default PostComp;
